047afffa4325f0fe8a0f6ba1263171a193550b89
alinmosery7 <alinmosery7@gmail.com>
2026-02-10 14:02:05 +0200

    Update route53_manager.py

--- src/resources/route53_manager.py
+++ src/resources/route53_manager.py
@@ -1,16 +1,17 @@
 import boto3
 import time
 from botocore.exceptions import ClientError
-from src.utils.helper import get_standard_tags
+from src.utils.helper import get_standard_tags, PROJECT_TAG
 
 
 def create_hosted_zone(domain_name):
-    # יוצר 'אזור ניהול' (Hosted Zone) לדומיין ב-Route53.
+    
+   # Creates a new Route 53 Hosted Zone (Domain 'bucket').
+    
     client = boto3.client('route53')
-    #AWS דורש איידי לכל בקשה ליצירת זון
-    # כדי שלא ייצא מצב שנריץ את אותו הסקריפט פעמיים ויווצרו שניים שונים (ונאלץ לשלם כפול)
-    # אז אם נשלח בקשה עם אותו מספר סידורי, נבין שזו אותה בקשה ולא יהיו כפילויות
-    # אנחנו משתמשים בשעון (Timestamp) כדי לייצר מספר שתמיד משתנה.
+
+    # AWS דורש "מספר סימוכין" ייחודי לכל בקשה כדי למנוע כפילויות
+    # אנחנו משתמשים בשעון (Timestamp) כדי לייצר מספר שלא חוזר על עצמו
     timestamp_ref = str(int(time.time()))
     caller_ref = f"{domain_name}-{timestamp_ref}"
 
@@ -22,23 +23,21 @@ def create_hosted_zone(domain_name):
             Name=domain_name,
             CallerReference=caller_ref,
             HostedZoneConfig={
-                'Comment': 'Created by Alin-DevOps-CLI', # הערה שתופיע בקונסול
-                'PrivateZone': False  # False = דומיין ציבורי שחשוף לאינטרנט (כמו google.com)
+                'Comment': 'Created by Alin-DevOps-CLI',
+                'PrivateZone': False  # False = דומיין ציבורי
             }
         )
 
         # AWS מחזיר מזהה ארוך ומכוער כמו '/hostedzone/Z042345...'
         full_zone_id = response['HostedZone']['Id']
 
-        # נצטרך רק את האיידי כדי לעבוד איתתו בהמשך
+        # נצטרך רק את האיידי כדי לעבוד איתו בהמשך
         clean_zone_id = full_zone_id.split('/')[-1]
 
-        #ניצור פקודה חדשה להוספת טאגים
-        # לא קיימת אפשרות להוספת טאגים create_hosted_zone.
-        # לכן, חייבים לקרוא לפקודה נפרדת: change_tags_for_resource.
+        # 2. הוספת טאגים (פעולה נפרדת ב-Route53)
         print(f"DEBUG: Adding tags to zone {clean_zone_id}...")
         
-        tags = get_standard_tags() # מביא את הטאג CreatedBy: 
+        tags = get_standard_tags() # מביא את הטאג CreatedBy
         client.change_tags_for_resource(
             ResourceType='hostedzone',
             ResourceId=clean_zone_id,
@@ -55,13 +54,9 @@ def create_hosted_zone(domain_name):
 
 
 def create_dns_record(zone_id, record_name, target_ip):
-    
-   # יוצר רשומת A (חיבור בין שם ל-IP).
-   # הפונקציה מקבלת:
-   # 1. zone_id המזהה של הדומיין 
-   # 2. record_name - השם המלא (למשל www.mysite.com)
-   # 3. target_ip - הכתובת של השרת שיצרנו (למשל 54.21.10.1)
-    
+    """
+    Creates an 'A Record' linking a name (record_name) to an IP (target_ip).
+    """
     client = boto3.client('route53')
 
     try:
@@ -74,21 +69,11 @@ def create_dns_record(zone_id, record_name, target_ip):
                 'Comment': 'Created via CLI',
                 'Changes': [
                     {
-                        # --- הסבר על UPSERT ---
-                        # Action יכול להיות: CREATE, DELETE, UPSERT.
-                        # אנחנו בוחרים UPSERT (שילוב של Update + Insert).
-                        # אם הרשומה לא קיימת -> הוא יוצר אותה.
-                        # אם הרשומה כבר קיימת -> הוא מעדכן אותה ל-IP החדש.
-                        # זה מונע שגיאות של "Record already exists".
-                        'Action': 'UPSERT', 
+                        'Action': 'UPSERT',  # Create or Update (דרוס אם קיים)
                         'ResourceRecordSet': {
                             'Name': record_name,
-                            'Type': 'A',  # סוג הרשומה: A = Address (כתובת IP)
-                            
-                            # TTL (Time To Live):
-                            # כמה זמן (בשניות) ה-DNS בעולם יזכור את הכתובת הזו לפני שיבדוק שוב.
-                            # שמנו 300 שניות (5 דקות) - זה זמן טוב לפיתוח.
-                            'TTL': 300,
+                            'Type': 'A',  # סוג הרשומה: A = Address
+                            'TTL': 300,   # זמן חיים במטמון (5 דקות)
                             'ResourceRecords': [{'Value': target_ip}]
                         }
                     }
@@ -99,3 +84,45 @@ def create_dns_record(zone_id, record_name, target_ip):
 
     except ClientError as e:
         return False, f"Error creating record: {e}"
+
+
+def list_hosted_zones():
+ 
+  #  Lists only hosted zones created by this CLI (filtering by tag).
+   
+    client = boto3.client('route53')
+    project_value = PROJECT_TAG['Value']
+    
+    try:
+        # 1. משיכת כל הזונס
+        response = client.list_hosted_zones()
+        all_zones = response['HostedZones']
+        my_zones = []
+
+        print(f"DEBUG: Scanning {len(all_zones)} zones for tags...")
+
+        # 2. סינון לפי תגיות
+        for zone in all_zones:
+            zone_id = zone['Id'].split('/')[-1] # ניקוי ה-ID
+            zone_name = zone['Name']
+            
+            try:
+                tags_response = client.list_tags_for_resource(
+                    ResourceType='hostedzone',
+                    ResourceId=zone_id
+                )
+                
+                tags = tags_response['ResourceTagSet']['Tags']
+                for tag in tags:
+                    if tag['Key'] == 'CreatedBy' and tag['Value'] == project_value:
+                        my_zones.append(f"{zone_name} (ID: {zone_id})")
+                        break
+            
+            except ClientError:
+                continue
+
+        return my_zones
+
+    except ClientError as e:
+        print(f"Error listing zones: {e}")
+        return []
